e8640cb58d79b47b933d2dd0b2a453020100fc3f
WDenny <denny.weis@web.de>
2022-02-28 14:29:18 +0100

    Website

---
+++ php/Startseite.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="de">
+	<head>
+		<meta charset="UTF-8">
+		<title>Startseite</title>
+		<link rel="stylesheet" href="../css/style.css">
+	</head>
+	<body>
+		
+
+			<h1>Startseite</h1>
+
+		
+		<div>
+
+			<h2>Über uns</h2>
+			<p>Seit eh und je sind wir die Nummer 1....</p>
+
+		</div>
+
+		<div>
+
+			<h2>Das könnte Ihnen gefallen:</h2>
+			
+
+		</div>
+
+		
+		
+
+		<div class="productcard">
+			<div class="product3">
+				<img class="productImage" src="https://m.media-amazon.com/images/I/71EUh47hkHL._AC_UL480_FMwebp_QL65_.jpg">
+				<div class="frame">
+					<h3 class="productsh3">Produkttitel</h3>
+					<p class="subtitle"> Sehr knappe und deskriptive Produktbeschreibung die sich auf maximal 3-4 Zeilen streckt. Der Benutzer bekommt kurz gesagt was der 
+						Artikel kann, um dann zu einer Kaufentscheidung zu kommen.
+					</p>
+					<button class="cartbutton">
+						<p class="buttonText">19,99€</p>
+						<img class="buttonImage" src="../img/cartIn.png">
+					</button>
+				</div>
+			</div>
+		</div>
+		<div class="productcard">
+			<div class="product4">
+				<img class="productImage" src="https://m.media-amazon.com/images/I/71EUh47hkHL._AC_UL480_FMwebp_QL65_.jpg">
+				<div class="frame">
+					<h3 class="productsh3">Produkttitel</h3>
+					<p class="subtitle"> Sehr knappe und deskriptive Produktbeschreibung die sich auf maximal 3-4 Zeilen streckt. Der Benutzer bekommt kurz gesagt was der 
+						Artikel kann, um dann zu einer Kaufentscheidung zu kommen.
+					</p>
+					<button class="cartbutton">
+						<p class="buttonText">19,99€</p>
+						<img class="buttonImage" src="../img/cartIn.png">
+					</button>
+				</div>
+			</div>
+		</div>	
+	</body>
+</html>
